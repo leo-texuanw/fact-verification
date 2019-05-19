d22537c0b2d36ea9f633f580e1d6197afcb214af
comeonleo <wutexuanleo@outlook.com>
2019-05-19 13:22:48 +1000

    impl word embedding functions with glove

--- vectorise_glove.py
+++ vectorise_glove.py
@@ -1,84 +1,61 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from os import listdir
+import csv
+from time import time
 from os.path import join
 from functools import partial, reduce
-from multiprocessing import Pool
 
-from time import time
-from tqdm import tqdm
-
-import csv
 import pandas as pd
 import numpy as np
 
-def load_glove(path):
-    return pd.read_csv(path, sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
+TOOL_PATH = './tools'
+GLOVE_840 = 'glove.840B.300d.txt'
 
-def load_corpus(file_path, file_name):
-    docs = {}
-    with open(join(file_path, file_name), 'r') as f:
-        for line in f:
-            line = line.strip('\n').split(' ')
-            title = line[0]
-            docs[title] = docs.get(title, []) + line[2:]
-    return docs
+
+def load_glove(path):
+    return pd.read_csv(
+            path,
+            sep=" ",
+            index_col=0,
+            header=None,
+            quoting=csv.QUOTE_NONE)
+
 
 def get_word_vec(word, glove):
+    """ get word embedding of a word """
+
     try:
         return glove.loc[word].as_matrix()
-    except:
+    except KeyError as _e:
         return glove.loc['unk'].as_matrix()
 
 
-def doc_embedding(doc, glove):
-    word_embeddings = map(partial(get_word_vec, glove=glove), doc)
-    sum_doc_embedding = reduce(np.add, word_embeddings)
-    return sum_doc_embedding / len(doc)
+def words_avg_embedding(words: list, glove):
+    """ get average word embedding of a list of words """
+
+    word_embeddings = map(partial(get_word_vec, glove=glove), words)
+    sum_words_embedding = reduce(np.add, word_embeddings)
+    return sum_words_embedding / len(words)
 
 
-def process_corpus_with_glove(file_name, file_path, dest_path, glove):
-    with open(join(dest_path, file_name), 'w') as dest_f:
-        docs = load_corpus(file_path, file_name)
+def words_embedding(words: list, glove):
+    """ get concatenated word embeddings of a list of words """
 
-        for title, doc in tqdm(docs.items()):
-            doc = doc_embedding(doc, glove)
-            temp = " ".join(map(str, doc.tolist()))
-            dest_f.write(title + " " + temp + '\n')
-    print("finish file", file_name)
+    word_embeddings = map(partial(get_word_vec, glove=glove), words)
+    concat_words_embedding = np.concatenate(list(word_embeddings))
+    return concat_words_embedding
 
 
-TOOL = './tools'
-GLOVE_840 = 'glove.840B.300d.txt'
-COURPUS_PATH = './wiki-pages-text/'
-PROCESSED_CORPUS_GLOVE = './processed_wiki_glove'
+if __name__ == '__main__':
+    start = time()
+    glove = load_glove(join(TOOL_PATH, GLOVE_840))
+    print("load glove:", time() - start, "seconds")
+    print(len(glove), "lines in glove")
 
-start = time()
-glove = load_glove(join(TOOL, GLOVE_840))
-print("load glove:", time() - start, "seconds")
+    words = ['hello', 'world', '!']
+    words_avg_embedding = words_avg_embedding(words, glove)
+    assert(len(words_avg_embedding) == 300)
 
-# line = ['title', 'hello', 'world']
-# doc_embed = doc_embedding(line, glove)
-# print(len(doc_embed))
-# print(doc_embed)
-
-
-data_files = listdir(COURPUS_PATH)
-
-pool = Pool(processes=12)
-pool.map(partial(process_corpus_with_glove,
-                 file_path=COURPUS_PATH,
-                 dest_path=PROCESSED_CORPUS_GLOVE,
-                 glove=glove),
-         data_files)
-pool.close()
-pool.join()
-
-
-#process_corpus_with_glove(
-#    data_files[0],
-#    file_path=COURPUS_PATH,
-#    dest_path=PROCESSED_CORPUS_GLOVE,
-#    glove=glove)
-
+    words_embedding = words_embedding(words, glove)
+    assert(len(words_embedding) == 300 * len(words))
