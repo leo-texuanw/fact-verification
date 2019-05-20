fcfa839e78414e80abdaf17e90d99f4b9e39137c
comeonleo <wutexuanleo@outlook.com>
2019-05-20 13:31:43 +1000

    refactoring xdb_query

--- xdb_query.py
+++ xdb_query.py
@@ -5,13 +5,14 @@ import json
 import xapian
 
 
-def get_query(querystring, prefix=None):
+def _get_query(querystring, prefix=None):
 
     # Set up a QueryParser with a stemmer and suitable prefixes
     queryparser = xapian.QueryParser()
     queryparser.set_stemmer(xapian.Stem("en"))
     queryparser.set_stemming_strategy(queryparser.STEM_SOME)
 
+    query = None
     if not prefix:
         query = queryparser.parse_query(querystring)
     elif prefix.lower() == "title":
@@ -25,13 +26,16 @@ def get_query(querystring, prefix=None):
     return query
 
 
-def search(dbpath, query, offset=0, pagesize=10):
+def search(db_path, query_str, prefix=None, offset=0, pagesize=5):
     """ offset - defines starting point within result set
         pagesize - defines number of records to retrieve
     """
 
     # Open the database we're going to search.
-    db = xapian.Database(dbpath)
+    db = xapian.Database(db_path)
+
+    # get query
+    query = _get_query(query_str, prefix)
 
     # Use an Enquire object on the database to run the query
     enquire = xapian.Enquire(db)
@@ -40,23 +44,27 @@ def search(dbpath, query, offset=0, pagesize=10):
     # And print out something about each match
     matches = []
     for match in enquire.get_mset(offset, pagesize, None, None):
-        fields = json.loads(match.document.get_data())
-        # print(fields)
-        print("{rank:} #{docid:3d} {title:}\n {sentences:} \n".format(
-            rank=match.rank + 1,
-            docid=match.docid,
+        doc = match.document.get_data()
+        matches.append((match.rank+1, match.docid, doc))
+
+    return matches
+
+
+def print_matches(matches):
+    for rank, doc_id, match in matches:
+        fields = json.loads(match)
+        print("Rank: {rank:}\t Doc ID: #{docid:3d}\t Title: {title:}\n {sentences:} \n".format(
+            rank=rank,
+            docid=doc_id,
             title=fields.get('title', ''),
             sentences=fields.get('sentences', ''),
-            ))
-        matches.append(match.docid)
-
-    # Finally, make sure we log the query and displayed results
-    # support.log_matches(querystring, offset, pagesize, matches)
+        ))
 
 
 if __name__ == '__main__':
     DB_PATH = './xdb/wiki.db'
-    prefix = 'text'  # options: ['title', 'text', None]
-    querystring = "Kevin Kraus"
-    query = get_query(querystring, prefix)
-    search(DB_PATH, query)
+    prefix = 'title'  # options: ['title', 'text', None]
+    query_str = "Kevin Kraus"
+    matches = search(DB_PATH, query_str, prefix)
+
+    print_matches(matches)
